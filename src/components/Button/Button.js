b92f36d803975114bc7122abfe7e200ff19b114c
Eka Prasetia <ekaone3033@gmail.com>
2020-10-07 18:31:22 +0700

    add component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "@chakra-ui/core";
+
+export default function MyButton() {
+  return (
+    <div>
+      <Button variantColor="green">Button</Button>
+    </div>
+  );
+}
