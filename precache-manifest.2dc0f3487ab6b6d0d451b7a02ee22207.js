a0c0e836c7d9e7eabd3842cde360ad6c50cceeca
Eka Prasetia <ekaone3033@gmail.com>
2020-10-12 21:51:26 +0700

    Updates

---
+++ precache-manifest.2dc0f3487ab6b6d0d451b7a02ee22207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d57281fe759b013b0544710ccf92b1",
+    "url": "/Play-ChakraUI/index.html"
+  },
+  {
+    "revision": "ab3b55b5f833b3ce99b0",
+    "url": "/Play-ChakraUI/static/js/2.c29935e6.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/Play-ChakraUI/static/js/2.c29935e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8707711ac572f6a26993",
+    "url": "/Play-ChakraUI/static/js/main.171b0183.chunk.js"
+  },
+  {
+    "revision": "4730aad0254623092ec5",
+    "url": "/Play-ChakraUI/static/js/runtime-main.6126cb2c.js"
+  },
+  {
+    "revision": "992237b049694793ff676cf6e0570cea",
+    "url": "/Play-ChakraUI/static/media/building.992237b0.jpg"
+  }
+]);
